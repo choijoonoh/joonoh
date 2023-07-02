0b6c5f3d181056d5c12897ee7a0158d411cf2641
choijoonoh <dusduddls@gmail.com>
2023-07-03 00:26:41 +0900

    start project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <MyBtn>Banana</MyBtn>
+  <MyBtn 
+      :color=color
+       ><span style="color: red;">banana</span>
+      </MyBtn>
+  <MyBtn large
+      color ="royalblue"
+      >apple</MyBtn>
+  <MyBtn>cherry</MyBtn>
+  <button>Banana</button>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+
+export default {
+    components: {
+      MyBtn
+    },
+    data(){
+      return{
+        color : "#000"
+      }
+    }
+}
+</script>
+
+<style>
+  
+
+</style>
